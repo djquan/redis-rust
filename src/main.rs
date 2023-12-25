e0d04173be0b0a72657fc3bd2bfb8f8c0a4dd3f4
Dan Quan <dan@quan.io>
2023-12-25 12:02:00 -0800

    Respond with PONG

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,12 +8,19 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_response(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_response(mut stream: TcpStream) {
+    let message = "+PONG\r\n";
+    stream.write(message.as_bytes()).unwrap();
+    stream.shutdown(std::net::Shutdown::Both).unwrap();
 }
