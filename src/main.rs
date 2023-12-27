46627ef26f9b89be29ddf6f8584d843ceaece033
Dan Quan <dan@quan.io>
2023-12-26 16:36:13 -0800

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    listen_and_serve(listener);
+}
 
+fn listen_and_serve(listener: TcpListener) {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
@@ -25,8 +26,6 @@ fn handle_response(stream: TcpStream) {
     let reader = BufReader::new(&stream);
     let mut writer = BufWriter::new(&stream);
 
-    // iterate over stream and use the lines() method to get an iterator
-    println!("accepted new connection");
     for line_result in reader.lines() {
         let line = match line_result {
             Ok(line) => line.trim().to_uppercase(),
@@ -43,5 +42,38 @@ fn handle_response(stream: TcpStream) {
             }
             _ => {}
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::{Read, Write};
+    use std::net::{SocketAddr, TcpListener, TcpStream};
+    use std::thread;
+
+    use crate::listen_and_serve;
+
+    #[test]
+    fn ping_works() {
+        let addr = start_server();
+
+        let mut stream = TcpStream::connect(addr).expect("Failed to connect to server");
+        stream.write_all(b"*1\r\n$4\r\nPING\r\n").unwrap();
+
+        let mut buffer = [0; 1024];
+        let n_bytes = stream.read(&mut buffer).expect("Failed to read from stream");
+        let received = String::from_utf8_lossy(&buffer[..n_bytes]);
+        stream.shutdown(std::net::Shutdown::Both).unwrap();
+
+        assert_eq!(received, "+PONG\r\n");
+    }
+
+    fn start_server() -> SocketAddr {
+        let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+        let addr = listener.local_addr().expect("Failed to get local address");
+
+        thread::spawn(move || { listen_and_serve(listener); });
+
+        return addr;
     }
 }
